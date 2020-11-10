b9280f1a74e246666d1203ef0e4b8f0e8f6e3bd5
Marco Schmidt <mschmidtgit@protonmail.com>
2020-11-10 21:25:00 +0100

    move MessageDataTest to correct package

--- msgdata/src/test/java/messy/msgdata/formats/messy/MessageDataTest.java
+++ msgdata/src/test/java/messy/msgdata/formats/messy/MessageDataTest.java
@@ -13,14 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package messy.msgdata.formats.anews;
+package messy.msgdata.formats.messy;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
 import org.junit.Assert;
 import org.junit.Test;
-import messy.msgdata.formats.messy.MessageData;
 
 public class MessageDataTest
 {
