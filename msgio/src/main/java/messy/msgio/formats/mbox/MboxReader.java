231c2d61e886c532f6a1b6f38ca431211e09868c
Marco Schmidt <mschmidtgit@protonmail.com>
2020-11-11 18:13:08 +0100

    added mbox reader class

---
+++ msgio/src/main/java/messy/msgio/formats/mbox/MboxReader.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package messy.msgio.formats.mbox;
+
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import messy.msgdata.formats.mbox.MboxMessage;
+
+/**
+ * Read <a target="_top" href="http://www.faqs.org/rfcs/rfc822.html">RFC 822</a> style messages as
+ * {@link messages.io.mbox.MboxMessage} objects from a {@link java.io.Reader} in
+ * <a target="_top" href="http://www.qmail.org/qmail-manual-html/man5/mbox.html">mbox(5)</a> format.
+ *
+ * Call {@link #next()} repeatedly until it returns <code>null</code>.
+ *
+ * @author Marco Schmidt
+ */
+public class MboxReader implements AutoCloseable
+{
+  /**
+   * Is high level unquoting enabled by default?
+   */
+  public static final boolean DEFAULT_HIGH_LEVEL_FROM_UNQUOTING = true;
+  private static final String FROM = "From ";
+  private LineNumberReader in;
+  private boolean unquoteHighLevelFroms = DEFAULT_HIGH_LEVEL_FROM_UNQUOTING;
+  private String envelopeLine;
+
+  public MboxReader(Reader reader)
+  {
+    this(reader, DEFAULT_HIGH_LEVEL_FROM_UNQUOTING);
+  }
+
+  public MboxReader(Reader reader, boolean unquoteHigh)
+  {
+    in = new LineNumberReader(reader);
+    unquoteHighLevelFroms = unquoteHigh;
+  }
+
+  @Override
+  public void close() throws IOException
+  {
+    if (in != null)
+    {
+      in.close();
+      in = null;
+    }
+  }
+
+  public int getLineNumber()
+  {
+    return in.getLineNumber();
+  }
+
+  public boolean isEnvelopeLine(String line)
+  {
+    if (!line.startsWith(FROM))
+    {
+      return false;
+    }
+    final String[] parts = line.split(" ");
+    final String year = parts[parts.length - 1];
+    try
+    {
+      final int yearNumber = Integer.parseInt(year);
+      return yearNumber >= 1970 && yearNumber < 10000;
+    }
+    catch (final NumberFormatException nfe)
+    {
+      return false;
+    }
+  }
+
+  public MboxMessage next() throws IOException
+  {
+    String line;
+    // find envelope line starting with "From "
+    if (envelopeLine == null)
+    {
+      while (true)
+      {
+        line = in.readLine();
+        if (line == null)
+        {
+          return null;
+        }
+        if (isEnvelopeLine(line))
+        {
+          envelopeLine = line;
+          break;
+        }
+      }
+    }
+    final MboxMessage msg = new MboxMessage();
+    msg.setEnvelope(envelopeLine, in.getLineNumber());
+    envelopeLine = null;
+
+    readHeader(msg);
+    readBody(msg);
+
+    return msg;
+  }
+
+  private void readHeader(MboxMessage msg) throws IOException
+  {
+    String line;
+    final List<String> lines = new ArrayList<String>();
+    while ((line = in.readLine()) != null)
+    {
+      if ("".equals(line))
+      {
+        break;
+      }
+      lines.add(line);
+    }
+    msg.setHeaderLines(lines);
+  }
+
+  private void readBody(MboxMessage msg) throws IOException
+  {
+    final List<String> lines = new ArrayList<String>();
+    String line;
+    while ((line = in.readLine()) != null)
+    {
+      if (isEnvelopeLine(line))
+      {
+        envelopeLine = line;
+        break;
+      }
+      int index = 0;
+      while (index < line.length() && '>' == line.charAt(index))
+      {
+        index++;
+      }
+      if (index > 0 && line.startsWith(FROM, index))
+      {
+        boolean unquote;
+        if (unquoteHighLevelFroms)
+        {
+          unquote = true;
+        }
+        else
+        {
+          unquote = index == 1;
+        }
+        if (unquote)
+        {
+          line = line.substring(1);
+        }
+      }
+      lines.add(line);
+    }
+    msg.setBodyLines(lines);
+  }
+}
---
+++ msgio/src/test/java/messy/msgio/formats/mbox/MboxReaderTest.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package messy.msgio.formats.mbox;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import org.junit.Assert;
+import org.junit.Test;
+import messy.msgdata.formats.mbox.MboxMessage;
+
+public final class MboxReaderTest
+{
+  /**
+   * Load resource data from argument file to byte array.
+   *
+   * @param name
+   *          file name
+   * @return byte array with data
+   * @throws IOException
+   *           on read errors
+   */
+  public byte[] readData(final String name) throws IOException
+  {
+    InputStream in = null;
+    try
+    {
+      in = getClass().getResourceAsStream(name);
+      final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      final byte[] buffer = new byte[1024];
+      int numRead;
+      while (true)
+      {
+        numRead = in.read(buffer);
+        if (numRead >= 0)
+        {
+          baos.write(buffer, 0, numRead);
+        }
+        else
+        {
+          break;
+        }
+      }
+      return baos.toByteArray();
+    }
+    catch (final IOException ioe)
+    {
+      return null;
+    }
+    finally
+    {
+      if (in != null)
+      {
+        in.close();
+      }
+    }
+  }
+
+  public Reader open(final String name) throws IOException
+  {
+    final byte[] data = readData(name);
+    return new InputStreamReader(new ByteArrayInputStream(data), StandardCharsets.ISO_8859_1);
+  }
+
+  private MboxMessage read(String name) throws IOException
+  {
+    final Reader in = open(name);
+    final MboxReader reader = new MboxReader(in);
+    final MboxMessage msg = reader.next();
+    final int number = reader.getLineNumber();
+    Assert.assertTrue("Read at least one line.", number > 0);
+    reader.close();
+    reader.close();
+    return msg;
+  }
+
+  @Test
+  public void testEnvelope() throws IOException
+  {
+    final Reader in = open("main.mbox");
+    final MboxReader reader = new MboxReader(in);
+    final boolean envelopeLine = reader.isEnvelopeLine("");
+    final MboxMessage msg = reader.next();
+    reader.close();
+    Assert.assertFalse("Empty line is no envelope line.", envelopeLine);
+    Assert.assertNotNull("Message read.", msg);
+  }
+
+  @Test
+  public void testEnvelopeNoHighLevelUnquoting() throws IOException
+  {
+    final Reader in = open("main.mbox");
+    final MboxReader reader = new MboxReader(in, false);
+    final MboxMessage msg = reader.next();
+    reader.close();
+    Assert.assertNotNull("Message read.", msg);
+  }
+
+  @Test
+  public void testRegular() throws IOException
+  {
+    final MboxMessage msg = read("main.mbox");
+    Assert.assertNotNull("Message read.", msg);
+  }
+
+  @Test
+  public void testSecondEnvelope() throws IOException
+  {
+    final Reader in = open("secondenvelope.mbox");
+    final MboxReader reader = new MboxReader(in);
+    MboxMessage msg = reader.next();
+    Assert.assertNotNull("First message read.", msg);
+    msg = reader.next();
+    Assert.assertNotNull("Second message read.", msg);
+    reader.close();
+  }
+
+  @Test
+  public void testSecondEnvelopeTruncated() throws IOException
+  {
+    final Reader in = open("secondenvelopetruncated.mbox");
+    final MboxReader reader = new MboxReader(in);
+    MboxMessage msg = reader.next();
+    Assert.assertNotNull("First message read.", msg);
+    msg = reader.next();
+    Assert.assertNotNull("Second message read.", msg);
+    reader.close();
+  }
+
+  @Test
+  public void testNoEnvelope() throws IOException
+  {
+    final MboxMessage msg = read("noenvelope.mbox");
+    Assert.assertNull("No message because of missing envelope line.", msg);
+  }
+
+  @Test
+  public void test1969() throws IOException
+  {
+    final MboxMessage msg = read("1969.mbox");
+    Assert.assertNull("Envelope line with year 1969 leads to null message.", msg);
+  }
+
+  @Test
+  public void test10000() throws IOException
+  {
+    final MboxMessage msg = read("10000.mbox");
+    Assert.assertNull("Envelope line with year 10000 leads to null message.", msg);
+  }
+
+  @Test
+  public void testInvalidYear() throws IOException
+  {
+    final MboxMessage msg = read("invalidyear.mbox");
+    Assert.assertNull("Envelope line with year XYZ leads to null message.", msg);
+  }
+}
