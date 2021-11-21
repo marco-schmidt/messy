f901ba19db06259a2fa187c5ef8edd863b4fadef
Marco Schmidt <mschmidtgit@protonmail.com>
2021-11-21 20:35:52 +0100

    FEAT switch over from sync to async elastic bulk requests

--- msgsearch/src/main/java/messy/msgsearch/elastic/ElasticOutputProcessor.java
+++ msgsearch/src/main/java/messy/msgsearch/elastic/ElasticOutputProcessor.java
@@ -21,8 +21,12 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.bulk.BulkItemResponse;
 import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
@@ -39,20 +43,22 @@ import messy.msgio.output.OutputProcessor;
  *
  * @author Marco Schmidt
  */
-public class ElasticOutputProcessor extends OutputProcessor
+public class ElasticOutputProcessor extends OutputProcessor implements ActionListener<BulkResponse>
 {
   private static final Logger LOGGER = LoggerFactory.getLogger(ElasticOutputProcessor.class);
   private BulkRequest bulkRequest;
+  private final long bulkRequestWaitMillis = 1000L;
   private RestHighLevelClient client;
   private int currentBulkSize;
   private int currentMessages;
-  private long totalMessages;
   private long connectionTimeMillis;
   private String host = "localhost";
   private String indexDatePattern = "yyyy";
   private String indexPrefix = "msg-";
-  private int maxBulkSize = 3 * 1024 * 1024;
+  private int maxBulkSize = 8 * 1024 * 1024;
   private int port = 9200;
+  private long totalMessages;
+  private final AtomicInteger requestCounter = new AtomicInteger(0);
 
   public ElasticOutputProcessor()
   {
@@ -65,21 +71,43 @@ public class ElasticOutputProcessor extends OutputProcessor
   {
     if (client != null)
     {
+      // send out any remaining elements
       if (currentBulkSize > 0)
       {
         flushBulkRequest();
       }
+
+      // wait for all requests to terminate
+      while (true)
+      {
+        final int left = requestCounter.get();
+        if (left == 0)
+        {
+          break;
+        }
+        LOGGER.info("Waiting for {} bulk request(s) to finish.", value("num_requests", left));
+        try
+        {
+          Thread.sleep(bulkRequestWaitMillis);
+        }
+        catch (final InterruptedException e)
+        {
+          LOGGER.error("Failed to sleep in thread: {}", e.getMessage());
+        }
+      }
+
+      // actually close
       try
       {
         client.close();
         final long millis = System.currentTimeMillis() - connectionTimeMillis;
-        LOGGER.info("Closed connection to Elastic server '{}:{}', sent {} message(s) in {} ms.", value("host", host),
-            value("port", Integer.valueOf(port)), value("total_messages", Long.valueOf(totalMessages)),
-            value("duration_ms", Long.valueOf(millis)));
+        LOGGER.info("Closed connection to {} server '{}:{}', sent {} message(s) in {} ms.",
+            value("server_type", getServerType()), value("host", host), value("port", Integer.valueOf(port)),
+            value("total_messages", Long.valueOf(totalMessages)), value("duration_ms", Long.valueOf(millis)));
       }
       catch (final IOException e)
       {
-        LOGGER.error("Failure closing Elastic client: {}.", e.getMessage());
+        LOGGER.error("Failure closing {} client: {}.", value("server_type", getServerType()), e.getMessage());
       }
       client = null;
     }
@@ -87,17 +115,15 @@ public class ElasticOutputProcessor extends OutputProcessor
 
   public void connect()
   {
-    if (client != null)
-    {
-      close();
-    }
+    close();
     client = new RestHighLevelClient(RestClient.builder(new HttpHost(host, port)));
     bulkRequest = new BulkRequest();
     currentBulkSize = 0;
     currentMessages = 0;
     totalMessages = 0;
     connectionTimeMillis = System.currentTimeMillis();
-    LOGGER.info("Connected to Elastic server '{}:{}'.", value("host", host), value("port", Integer.valueOf(port)));
+    LOGGER.info("Connected to {} server '{}:{}'.", value("server_type", getServerType()), value("host", host),
+        value("port", Integer.valueOf(port)));
   }
 
   public String determineIndexName(Message msg)
@@ -126,21 +152,22 @@ public class ElasticOutputProcessor extends OutputProcessor
 
   public void flushBulkRequest()
   {
-    try
-    {
-      final long now = System.currentTimeMillis();
-      client.bulk(bulkRequest, RequestOptions.DEFAULT);
-      LOGGER.info("Sent {} messages to Elastic server, {} characters in {} ms.",
-          value("num_messages", Integer.valueOf(currentMessages)), value("num_chars", Integer.valueOf(currentBulkSize)),
-          value("duration_ms", Long.valueOf(System.currentTimeMillis() - now)));
-    }
-    catch (final IOException e)
-    {
-      LOGGER.error("Failure flushing bulk request to Elastic: {}.", e.getMessage());
-    }
+    final long now = System.currentTimeMillis();
+    requestCounter.incrementAndGet();
+    client.bulkAsync(bulkRequest, RequestOptions.DEFAULT, this);
+    LOGGER.info("Sending {} messages to {} server, {} characters in {} ms.",
+        value("num_messages", Integer.valueOf(currentMessages)), value("server_type", getServerType()),
+        value("num_chars", Integer.valueOf(currentBulkSize)),
+        value("duration_ms", Long.valueOf(System.currentTimeMillis() - now)));
+
     bulkRequest = new BulkRequest();
     currentBulkSize = 0;
     currentMessages = 0;
+  }
+
+  public String getServerType()
+  {
+    return "Elastic";
   }
 
   public int getMaxBulkSize()
@@ -191,5 +218,41 @@ public class ElasticOutputProcessor extends OutputProcessor
   public void setPort(int port)
   {
     this.port = port;
+  }
+
+  @Override
+  public void onFailure(Exception e)
+  {
+    LOGGER.error("Problem sending messages to server: {}", e.getMessage());
+    requestCounter.decrementAndGet();
+  }
+
+  @Override
+  public void onResponse(BulkResponse response)
+  {
+    final long ingestTook = response.getIngestTook().getMillis();
+    final long took = response.getTook().getMillis();
+    final BulkItemResponse[] items = response.getItems();
+    final int numMessages = items.length;
+    LOGGER.info("Sent {} messages to {} server, ingest {} ms and bulk execution {} ms.",
+        value("num_messages", Integer.valueOf(numMessages)), value("server_type", getServerType()),
+        value("ingest_duration_ms", Long.valueOf(ingestTook)), value("bulk_duration_ms", Long.valueOf(took)));
+
+    if (response.hasFailures())
+    {
+      for (final BulkItemResponse bir : items)
+      {
+        if (bir.isFailed())
+        {
+          final String id = bir.getId();
+          final String index = bir.getIndex();
+          final String message = bir.getFailureMessage();
+          LOGGER.error("Failed bulk request item {} in index {}, message: '{}'.", value("id", id),
+              value("index", index), value("message", message));
+        }
+      }
+    }
+
+    requestCounter.decrementAndGet();
   }
 }
